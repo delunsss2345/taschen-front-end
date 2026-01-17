e201c44cd83b466cb3a94fee0edcaabcc429a1b6
Phạm Thanh Huy <huyp7922@gmail.com>
2026-01-17 12:49:05 +0700

    feat(auth): add auth API response types
    - Add generic ApiResponse<T> for consistency
    - Define types for login, register, refresh data
    - Create combined response types (e.g., LoginResponse)
    - Include void types for logout, verify, change pass

---
+++ src/types/auth.response.ts
@@ -0,0 +1,47 @@
+export type ApiResponse<T> = {
+  error: string | null;
+  message: string;
+  statusCode: number;
+  data: T;
+};
+
+export type RegisterResponseData = {
+  id: number;
+  email: string;
+  verifyToken: string;
+  firstName: string;
+  lastName: string;
+  gender: string | null;
+  phoneNumber: string | null;
+  active: boolean;
+  roles: string[];
+  addresses: unknown[] | null;
+};
+
+
+export type LoginResponseData = {
+  userId: number;
+  email: string;
+  expiresIn: number;
+  tokenType: string;
+  refreshToken: string;
+  accessToken: string;
+  roles: string[];
+};
+
+export type RefreshTokenResponseData = {
+  userId: number;
+  email: string;
+  expiresIn: number;
+  tokenType: string;
+  refreshToken: string;
+  accessToken: string;
+  roles: string[];
+};
+
+export type LoginResponse = ApiResponse<LoginResponseData>;
+export type RefreshResponse = ApiResponse<RefreshTokenResponseData>;
+export type RegisterResponse = ApiResponse<RegisterResponseData>;
+export type LogoutResponse = void;
+export type ChangePasswordResponse = void;
+export type VerifyAccountResponse = void;
